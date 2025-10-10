b952b510823e99ec52c1a2fc45d27c5423654677
Christopher Yang <chryang01@gmail.com>
2025-10-10 14:58:30 -0500

    created User model

---
+++ src/main/java/com/revature/spring/jpa/spring_jpa/models/User.java
@@ -0,0 +1,29 @@
+package com.revature.spring.jpa.spring_jpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+
+    public String firstName;
+
+    public String lastName;
+
+    public String email;
+
+    public String password;
+}
